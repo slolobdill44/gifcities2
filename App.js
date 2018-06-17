be9b6a49ce6acd6d59679096fe96ebb4cd7dd9b1
Adrian Lobdill <slolobdill44@gmail.com>
2018-06-17 13:19:05 -0700

    componentizes splash content

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import { Alert,
 import RNFetchBlob from 'react-native-fetch-blob';
 import Header from './header';
 import ImageResult from './image_result';
+import SplashContent from './splash_content'
 
 export default class App extends Component {
   constructor(props) {
@@ -86,20 +87,7 @@ export default class App extends Component {
         <Header
           onChange={(value) => this.setState({searchQuery: value})}
           submitSearch={this.submitSearch}/>
-        <View style={styles.splashAlign}>
-          <View style={styles.splashContainer}>
-            <View style={styles.upperSplashContainer}>
-              <Text style={styles.mainTitleText}>GifCities</Text>
-              <Text style={styles.mainTitleDescription}>The GeoCities Animated GIF Search Engine</Text>
-              <Text style={styles.mainTitleSubText}>From the Internet Archive</Text>
-              <Image 
-                source={require('./images/spinning-internetarchive.gif')}/>
-            </View>
-            <View style={styles.lowerSplashContainer}>
-              <Text style={styles.lowerSplashText}>GifCities is a special project of the Internet Archive to celebrate 20 years of preserving the web. Internet Archive is a non-profit digital library of millions of free books, movies, software, music, websites, and more. Please donate to help us in our efforts to provide “Universal Access to All Knowledge” including GIFs.</Text>
-            </View>
-          </View>
-        </View>
+        <SplashContent />
       </View>
     ); 
   }
@@ -118,48 +106,6 @@ const styles = StyleSheet.create({
         paddingTop: 10
       }
     })
-  },
-  splashAlign: {
-    flex: 1,
-    flexDirection: 'row',
-    width: '95%',
-    alignSelf: 'center'
-  },
-  splashContainer: {
-    height: '90%',
-    alignSelf: 'flex-end',
-    justifyContent: 'space-between'
-  },
-  upperSplashContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'space-between'
-  },
-  mainTitleText: {
-    fontSize: 72,
-    letterSpacing: 1,
-    fontWeight: 'bold',
-    color: 'teal',
-    fontFamily: 'serif'
-  },
-  mainTitleDescription: {
-    fontSize: 24,
-    color: '#999',
-    textAlign: 'center'
-  },
-  mainTitleSubText: {
-    fontSize: 16,
-    color: 'black'
-  },
-  lowerSplashContainer: {
-    height: '35%',
-    flexDirection: 'row',
-    alignItems: 'center'
-  },
-  lowerSplashText: {
-    fontSize: 18,
-    color: 'black',
-    textAlign: 'center'
   },
   contentWrapper: {
     top: 0,
