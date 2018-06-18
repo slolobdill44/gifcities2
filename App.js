7bbc15000d91809c8f38be6e961c92d973beff1e
Adrian Lobdill <slolobdill44@gmail.com>
2018-06-18 08:13:38 -0700

    starts componentizing search results

--- App.js
+++ App.js
@@ -87,7 +87,34 @@ export default class App extends Component {
         <Header
           onChange={(value) => this.setState({searchQuery: value})}
           submitSearch={this.submitSearch}/>
-        <SplashContent />
+                { this.state.searchLoading ? 
+          (<View style={styles.searchLoadingWrapper}>
+             <Image
+               style={styles.searchSpinner}
+               source={require('./images/loading_images/loading3.gif')}/>
+           </View>) : 
+          (<ScrollView
+             contentContainerStyle={styles.contentContainerStyle}
+             keyboardDismissMode='on-drag'>
+               {this.state.data.map((item, idx) => {
+                 return (
+                     <TouchableHighlight
+                       key={idx}
+                       onPress={() => this.saveToCameraRoll(item)}
+                       underlayColor='transparent'>
+                         <Image
+                           source={{uri: this.gifUrl(item.gif)}}
+                           style={{
+                             width: item.width > 350 ? undefined : item.width,
+                             height: item.height > 200 ? undefined : item.height,
+                             margin: 7,
+                             borderWidth: 10
+                           }}
+                         />
+                     </TouchableHighlight>
+                   )
+               })}
+           </ScrollView>) }
       </View>
     ); 
   }
---
+++ search_results.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { View, 
+		Text, 
+		Image, 
+		StyleSheet,
+		TouchableHighlight,
+		ScrollView } from "react-native";
+
+class SearchResults extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+
+  	return (
+
+  	)
+  }
+}
+
+const styles = StyleSheet.create({
+	
+})
+
+export default SearchResults;
